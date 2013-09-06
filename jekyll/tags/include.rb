995b988dc96c77fdb71b36cab4241f76f6b04499
Roger Xu <roger.xu.2007@gmail.com>
2013-09-06 12:25:40 +0800

    Fix encoding issue for Jekyll.

--- jekyll/tags/include.rb
+++ jekyll/tags/include.rb
@@ -62,7 +62,7 @@ eos
         Dir.chdir(includes_dir) do
           choices = Dir['**/*'].reject { |x| File.symlink?(x) }
           if choices.include?(@file)
-            source = File.read(@file)
+            source = File.read(@file, :encoding => "utf-8")
             partial = Liquid::Template.parse(source)
 
             context.stack do
